007f4875a6670e685be6ae29be6ea93e4137dae3
axy-moon <augxy666@gmail.com>
2023-07-23 23:19:34 +0530

    User Module I

--- community/home/urls.py
+++ community/home/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name="home"),
+    path('register',views.register,name="register"),
+    path('login',views.login,name="login"),
+    path('verify',views.verify,name="verify")
+
+
 ]
--- community/home/views.py
+++ community/home/views.py
@@ -1,6 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import auth,User
+from .models import *
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.core.mail import send_mail
+import random
 
 # Create your views here.
 
 def home(request):
     return render(request,"index.html")
+
+def register(request):
+    global email,username
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+
+        if(password==cpassword):
+            User.objects.create_user(username=username,email=email,password=password)
+            return redirect('verify')
+        else:
+            return HttpResponse("Password doesn't match")
+        
+    else:
+        return render(request,"register.html")
+
+def login(request):
+    return render(request,"login.html")
+
+def verify(request):
+    global otp
+    if(request.method == 'POST'):
+        entered_otp = request.POST['otp']
+        print(otp)
+        if (int(entered_otp)==int(otp)):
+            return redirect('login')
+        else:
+            d = User.objects.latest()
+            d.delete()
+            return HttpResponse("failed")
+    otp = random.randrange(1000,9999)
+    mail_verify(username,email,otp)
+    print(otp)
+    return render(request,"verify.html")
+
+
+def mail_verify(username,email,otp):
+    subject = '' + str(otp)
+    message = f'Hi {username}, Welcome to Sharity. Your OTP for verifying your account is {otp}'
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = [email, ]
+    send_mail( subject, message, email_from, recipient_list )
+    return True
+
